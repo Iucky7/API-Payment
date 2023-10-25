62e51966a8a4d7c849707be6fd076c6a7310898e
bugisuki8 <bugisuki8@gmail.com>
2023-10-25 23:31:30 +0800

    add middleware

---
+++ delivery/middleware/auth_middleware.go
@@ -0,0 +1,42 @@
+package middleware
+
+import (
+	"api-payment/utils/security"
+	"net/http"
+	"strings"
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
+
+type authHeader struct {
+	AuthorizationHeader string `header:"Authorization"`
+}
+
+func AuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var header authHeader
+		if err := c.ShouldBindHeader(&header); err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{"message": "Unauthorized"})
+			c.Abort()
+			return
+		}
+		token := strings.Replace(header.AuthorizationHeader, "Bearer ", "", 1)
+		if token == "" {
+			c.JSON(http.StatusUnauthorized, gin.H{"message": "Unauthorized"})
+			c.Abort()
+			return
+		}
+		claims, err := security.VerifyAccessToken(token)
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{
+				"message": "Unauthorized",
+				"error" : err.Error(),
+			})
+			c.Abort()
+			return
+		}
+		fmt.Println("Claims :",claims["username"])
+		c.Next()
+	}
+}
